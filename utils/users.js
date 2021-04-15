eab0da0707cec9281e8172d0b38cd40e9a54ec74
Abhay Sardhara <40565504+Abhaysardhara@users.noreply.github.com>
2021-04-15 17:03:44 +0530

    Add files via upload

---
+++ utils/users.js
@@ -0,0 +1,71 @@
+const users = [];
+
+function userJoin(id, username, room, score) { 
+    const user = {
+        id : id,
+        username : username,
+        room : room,
+        score : score
+    };
+    users.push(user);
+    return user;
+}
+
+function userLeave(id) {
+    const idx = users.findIndex(user => user.id === id);
+    if(idx !== -1) {
+        return users.splice(idx, 1)[0];
+    }
+}
+
+function getRoomUsers(room) {
+    return users.filter(user => user.room === room);
+}
+
+function updateRoomUsersPoint(username, score, room) {
+    let idx = users.findIndex(user => ((user.username == username) && (user.room == room)));
+    users[idx].score += score;
+}
+
+function substractRoomUsersPoint(username, score, room) {
+    let idx = users.findIndex(user => ((user.username == username) && (user.room == room)));
+    users[idx].score -= score;
+}
+
+function userLen(room) {
+    return users.filter(user => user.room==room).length;
+}
+
+function getWinner(room) {
+    let maxi=-1;
+    users.forEach(ele => {
+        if((ele.room == room) && (ele.score >= maxi)) {
+            maxi = ele.score;
+        }
+    });
+    return users.filter(user => ((user.score === maxi) && (user.room == room)));
+}
+
+Array.prototype.remove = function(value) {
+    for (var i = this.length; i--; )
+    {
+        if (this[i].room === value) {
+            this.splice(i, 1);
+        }
+    }
+}
+
+function resetUsers(room) {
+    users.remove(room);
+}
+
+module.exports = {
+    userJoin,
+    userLeave,
+    getRoomUsers,
+    updateRoomUsersPoint,
+    substractRoomUsersPoint,
+    userLen,
+    getWinner,
+    resetUsers
+};
